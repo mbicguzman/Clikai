8f2c0ab2e9653dd8260f247fbaced78b3d1baf40
mbicguzman <cguzman@merchantsbankofindiana.com>
2024-02-13 11:57:36 -0500

    Added general/service down error

--- ErrorEmailLogic.js
+++ ErrorEmailLogic.js
@@ -10,25 +10,40 @@ loanNumber = inputRecord.Opstatmt.LoanNumber;
 statementDate = inputRecord.Opstatmt.StatementDate;
 statementType = inputRecord.Opstatmt.UserDefType;
 
-
 errorCode = "";
 
-if (indexLocation = errorError.indexOf("already exists") > 0){
+if (errorStatusCode = 500 && errorMsg == "Unable to call service"){
+    errorCode = "General";
+    input.parameters.userEmail = inputRecord.userEmailId;
+    input.parameters.ccEmail = "charper@merchantsbankofindiana.com; cguzman@merchantsbankofindiana.com; ngajjela@merchantscapital.com";
+    input.parameters.subject = "Clik.ai Upload Error - Loan " + loanNumber;
+    input.parameters.body = "<head><style>table, th, td {border: 1px solid black;border-collapse: collapse;}th, td {padding-left: 10px;padding-right: 10px;}</style></head><body><p>Hello,</p><p>An unknown error occurred while sending your Operating Statement with the below parameters:</p><table><tr><td>Loan Number</td><td>" + loanNumber + 
+                        "</td></tr><tr><td>Collateral Code</td><td>" + propCode + "</td></tr><tr><td>Statement Date</td><td>" + statementDate + "</td></tr><tr><td>Statement Type</td><td>"+ statementType +"</td></tr></table>" +
+                        "<p>Please submit a ticket to BI in The Hopper.</p><p>Thanks,</p></body>";
+} else if (indexLocation = errorError.indexOf("already exists") > 0){
     errorCode = "Duplicate";
     input.parameters.userEmail = inputRecord.userEmailId;
-    input.parameters.subject = "Clik.ai Upload Error - Duplicate Operating Statement in PLM";
+    input.parameters.ccEmail = "";
+    input.parameters.subject = "Clik.ai Upload Error - Loan " + loanNumber;
     input.parameters.body = "<head><style>table, th, td {border: 1px solid black;border-collapse: collapse;}th, td {padding-left: 10px;padding-right: 10px;}</style></head><body><p>Hello,</p><p>A duplicate Operating Statement already exists in PLM with the below parameters:</p><table><tr><td>Loan Number</td><td>" + loanNumber + 
                         "</td></tr><tr><td>Collateral Code</td><td>" + propCode + "</td></tr><tr><td>Statement Date</td><td>" + statementDate + "</td></tr><tr><td>Statement Type</td><td>"+ statementType +"</td></tr></table>" +
                         "<p>This can be resolved by deleting the duplicate in PLM. Please submit a BI ticket if issues persist.</p><p>Thanks,</p></body>";
-} else if (indexLocation = errorError.indexOf("Collateral Code not found in database") > 0) {
+} else if (indexLocation = errorError.indexOf("not found in database") > 0) {
     errorCode = "Collateral";
     input.parameters.userEmail = inputRecord.userEmailId;
-} else if (errorStatusCode = 500 && errorMsg == "Unable to call service") {
-    errorCode = "ServiceDown";
-    input.parameters.userEmail = inputRecord.userEmailId;
+    input.parameters.ccEmail = "";
+    input.parameters.subject = "Clik.ai Upload Error - Loan " + loanNumber;
+    input.parameters.body = "<head><style>table, th, td {border: 1px solid black;border-collapse: collapse;}th, td {padding-left: 10px;padding-right: 10px;}</style></head><body><p>Hello,</p><p>While sending your Operating Statement with the below parameters:</p><table><tr><td>Loan Number</td><td>" + loanNumber + 
+                        "</td></tr><tr><td>Collateral Code</td><td>" + propCode + "</td></tr><tr><td>Statement Date</td><td>" + statementDate + "</td></tr><tr><td>Statement Type</td><td>"+ statementType +"</td></tr></table>" +
+                        "<p>There was a problem with that folder's API integration. Please submit a BI Ticket in The Hopper. If the spread urgently needs to be uploaded, please use the Model Integration method to upload.</p><p>Thanks,</p></body>";
 } else {
     errorCode = "General";
     input.parameters.userEmail = inputRecord.userEmailId;
+    input.parameters.ccEmail = "charper@merchantsbankofindiana.com; cguzman@merchantsbankofindiana.com; ngajjela@merchantscapital.com";
+    input.parameters.subject = "Clik.ai Upload Error - Loan " + loanNumber;
+    input.parameters.body = "<head><style>table, th, td {border: 1px solid black;border-collapse: collapse;}th, td {padding-left: 10px;padding-right: 10px;}</style></head><body><p>Hello,</p><p>An unknown error occurred while sending your Operating Statement with the below parameters:</p><table><tr><td>Loan Number</td><td>" + loanNumber + 
+                        "</td></tr><tr><td>Collateral Code</td><td>" + propCode + "</td></tr><tr><td>Statement Date</td><td>" + statementDate + "</td></tr><tr><td>Statement Type</td><td>"+ statementType +"</td></tr></table>" +
+                        "<p>Please submit a ticket to BI in The Hopper.</p><p>Thanks,</p></body>";
 };
 
 
